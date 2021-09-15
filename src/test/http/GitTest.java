d9d6c3845f451c559b744f169948449a649841c1
2kijung <88864174+2kijung@users.noreply.github.com>
2021-09-15 14:52:03 +0900

    Update GitTest.java

--- src/test/http/GitTest.java
+++ src/test/http/GitTest.java
@@ -3,4 +3,5 @@ package test.http;
 public class GitTest {
 	private static final String DESC="GIT TEST용 클래습니다.";
 	private static final String CREDENTAL="GIT 인증 테스트";
+	private static final String GITHUB="원격 리포지토리에서 직접 수정";
 }
